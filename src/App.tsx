192ae8a1be53213a2cdb0a4507b5915f49b55c80
Maryamjbl <maryamjabali111@gmail.com>
2026-05-10 22:28:23 +0530

    First Comit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Course from "./Course";
+
+function App(){
+
+  return (
+    <div className="App">
+      <h1> Courses</h1>
+      <Course />
+    </div>
+  );
+} export default App;
---
+++ src/Course.tsx
@@ -0,0 +1,15 @@
+function Course() {
+return(
+    <div>
+        <h2>Course List</h2>
+       <ul>
+        <li>MERN Stack</li>
+        <li>Python</li>
+        <li>Java</li>
+        <li>Data Science</li>
+        <li>React</li>
+       </ul>
+    </div>
+)
+}
+export default Course;
